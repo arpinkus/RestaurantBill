64360ac437dd35d241493c9aaeb5c2fd7825a564
Abigail Pinkus <arpinkus@cpp.edu>
2022-03-03 00:01:31 -0800

    Update main

--- main.c
+++ main.c
@@ -3,56 +3,51 @@
 #include <time.h>
 
 int randomNumber() {
-    for (int i = 0; i > 1; i++) {
-        int num = (rand() % (4 - 1 + 1)) + 1;
-        return num;
-    }
+    time_t t;
+    srand((unsigned) time(&t));
+    int num = rand() % 4;
+    printf("num: %d\n",num);
+    return num;
 }
 
-float mealCost() {
-    float mealCost;
-    
-    srand(time(0));
+void main() {
+    double mealCost;
+    double saladCost;
+    double soupCost;
+    double sandwitchCost;
+    double pizzaCost;
 
     int random = randomNumber();
     printf("%d\n", random);
 
-    switch(randomNumber()) {
+    switch(random) {
         case 1:
-        double saladCost = 9.95;
-        mealCost = saladCost;
+        saladCost = 9.95;
+        mealCost = (double) saladCost;
         printf("Salad: $%u\n", saladCost);
         break;
         case 2:
-        double soupCost = 4.55;
-        mealCost = soupCost;
+        soupCost = 4.55;
+        mealCost = (double) soupCost;
         printf("Soup: $%u\n", soupCost);
         break;
         case 3:
-        double sandwitchCost = 13.25;
-        mealCost = sandwitchCost;
+        sandwitchCost = 13.25;
+        mealCost = (double) sandwitchCost;
         printf("Sandwitch: $%u\n", sandwitchCost);
         break;
         case 4:
-        double pizzaCost = 22.35;
-        mealCost = pizzaCost;
+        pizzaCost = 22.35;
+        mealCost = (double) pizzaCost;
         printf("Pizza: $%u\n", pizzaCost);
         break;
         default:
         printf("Not equal to a menu item! Something went wrong!");
     }
-    return mealCost;
-}
-
-void tax() {
+    
     float tax;
     float taxAmount;
-    char *percent = '%';
+    char percent = '%';
     scanf("%d", tax);
-    printf("Tax: %d%c\n", tax, &percent[0]);
-}
-
-void main() {
-    mealCost();
-    tax();
+    printf("Tax: %d%c\n", tax, percent);
 }
