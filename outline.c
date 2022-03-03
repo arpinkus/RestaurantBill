78da9aa2b7f2daf3d651ad8e5db0e07cc8d65eaa
Abigail Pinkus <arpinkus@cpp.edu>
2022-03-02 17:23:56 -0800

    Outline created

---
+++ outline.c
@@ -0,0 +1,22 @@
+/* // Compute tax and tip on a restaurant bill
+
+// const float tax;
+// const float tip;
+// pass tip and tax into main() as arguments
+// float mealCost;
+// float taxAmount;
+// float tipAmount;
+// float or double totalBill;
+
+
+// Use random number between 1 and 4
+// then use switch to hold salad, soup, sandwich, and pizza.
+// use const floats for the meal type costs above.
+// return the number from the switch as mealCost
+
+// then calculate the taxAmount using mealCost and const float tax
+// calculate the tipAmount using mealCost and const float tip
+// Finally calculate the totalBill by adding tip, tax, and mealCost respectively
+// Then print taxAmount, tipAmount, mealCost, and totalBill
+
+ */
